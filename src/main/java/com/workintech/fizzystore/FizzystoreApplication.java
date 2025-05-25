4d2cc0cec7669642254a4790905fea99d1e6da47
Bahar <80460166+baharkyc@users.noreply.github.com>
2025-05-25 16:34:35 +0300

    Initial commit

---
+++ src/main/java/com/workintech/fizzystore/FizzystoreApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.fizzystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzystoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/fizzystore/FizzystoreApplicationTests.java
@@ -0,0 +1,9 @@
+package com.workintech.fizzystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzystoreApplicationTests {
+
+}
