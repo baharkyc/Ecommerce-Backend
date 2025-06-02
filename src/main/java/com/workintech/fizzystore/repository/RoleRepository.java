c36ac55e1f7ab39db77c1649b009fd9b88c553aa
baharkyc <baharokuyucu@gmail.com>
2025-06-02 22:27:27 +0300

    feat: add custom query to UserRepository and RoleRepository

--- src/main/java/com/workintech/fizzystore/repository/RoleRepository.java
+++ src/main/java/com/workintech/fizzystore/repository/RoleRepository.java
@@ -3,7 +3,13 @@ package com.workintech.fizzystore.repository;
 
 import com.workintech.fizzystore.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    @Query("SELECT r FROM Role r WHERE r.code = :authority")
+    Optional<Role> findAuthority(@Param("authority") String authority);
 }
--- src/main/java/com/workintech/fizzystore/repository/UserRepository.java
+++ src/main/java/com/workintech/fizzystore/repository/UserRepository.java
@@ -2,6 +2,15 @@ package com.workintech.fizzystore.repository;
 
 import com.workintech.fizzystore.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT FROM User u WHERE u.email = :email")
+    Optional <User> findByEmail(@Param("email") String email);
+
+
 }
