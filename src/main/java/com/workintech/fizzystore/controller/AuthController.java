5f41b560591393c5b715c0fd45b769350c329a6b
baharkyc <baharokuyucu@gmail.com>
2025-06-06 22:34:42 +0300

    feat: add login endpoint

--- src/main/java/com/workintech/fizzystore/controller/AuthController.java
+++ src/main/java/com/workintech/fizzystore/controller/AuthController.java
@@ -1,10 +1,11 @@
 package com.workintech.fizzystore.controller;
 
-import com.workintech.fizzystore.dto.RegisterRequest;
+import com.workintech.fizzystore.dto.LoginRequestDto;
+import com.workintech.fizzystore.dto.LoginResponseDto;
+import com.workintech.fizzystore.dto.RegisterRequestDto;
 import com.workintech.fizzystore.dto.RegisterResponse;
 import com.workintech.fizzystore.service.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +20,20 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public RegisterResponse register(@Validated @RequestBody RegisterRequest request) {
+    public RegisterResponse register(@Validated @RequestBody RegisterRequestDto request) {
+
         authService.register(request.getName(),request.getEmail(), request.getPassword());
 
         return new RegisterResponse("User account created successfully.");
     }
+
+    @PostMapping("/login")
+    public LoginResponseDto login(@Validated @RequestBody LoginRequestDto request) {
+
+        authService.login(request.getEmail(), request.getPassword());
+
+        return new LoginResponseDto("Login successful.");
+    }
+
+
 }
