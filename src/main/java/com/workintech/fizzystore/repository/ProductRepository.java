832a6c9be5d185c19b3335176b1a27457dbe2268
baharkyc <baharokuyucu@gmail.com>
2025-06-06 22:35:59 +0300

    feat: add sorting logic to product repository

--- src/main/java/com/workintech/fizzystore/repository/ProductRepository.java
+++ src/main/java/com/workintech/fizzystore/repository/ProductRepository.java
@@ -3,5 +3,11 @@ package com.workintech.fizzystore.repository;
 import com.workintech.fizzystore.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByOrderByPriceAsc();
+    List<Product> findAllByOrderByPriceDesc();
+
 }
