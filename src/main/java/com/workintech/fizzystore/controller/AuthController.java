a35b27eda20512a4af5d068efa70315ffb4054ac
baharkyc <baharokuyucu@gmail.com>
2025-06-08 22:03:15 +0300

    feat: add token verify endpoint

--- src/main/java/com/workintech/fizzystore/controller/AuthController.java
+++ src/main/java/com/workintech/fizzystore/controller/AuthController.java
@@ -1,23 +1,28 @@
 package com.workintech.fizzystore.controller;
 
-import com.workintech.fizzystore.dto.LoginRequestDto;
-import com.workintech.fizzystore.dto.LoginResponseDto;
-import com.workintech.fizzystore.dto.RegisterRequestDto;
-import com.workintech.fizzystore.dto.RegisterResponse;
+import com.workintech.fizzystore.config.JwtUtil;
+import com.workintech.fizzystore.dto.*;
+import com.workintech.fizzystore.exceptions.FizzyStoreException;
 import com.workintech.fizzystore.service.AuthService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@AllArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    public AuthController (AuthService authService, JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+        this.authService = authService;
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
 
     @PostMapping("/register")
     public RegisterResponse register(@Validated @RequestBody RegisterRequestDto request) {
@@ -30,10 +35,20 @@ public class AuthController {
     @PostMapping("/login")
     public LoginResponseDto login(@Validated @RequestBody LoginRequestDto request) {
 
-        authService.login(request.getEmail(), request.getPassword());
+        return authService.login(request.getEmail(), request.getPassword());
 
-        return new LoginResponseDto("Login successful.");
     }
 
+    @GetMapping("/verify")
+    public VerifyResponseDto verifyToken(@RequestHeader("Authorization") String authHeader) {
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            return authService.verifyToken(authHeader);
+
+        } else {
+            throw new FizzyStoreException("Authorization header must start with Bearer", HttpStatus.UNAUTHORIZED);
+        }
+    }
 
 }
